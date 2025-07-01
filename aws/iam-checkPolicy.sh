10d9629c89b6d0231d50fb722efb1956b70ae042
cipolone95 <cipolone95@gmail.com>
2025-07-01 15:01:23 -0400

    updating iam check policy to include checking for inline policies

--- aws/iam-checkPolicy.sh
+++ aws/iam-checkPolicy.sh
@@ -27,19 +27,36 @@ if [[ -z "$PROFILE" ]]; then
 fi
 
 
-username="$(aws sts get-caller-identity --profile "$PROFILE" | jq -r '.Arn | split("/") | last')"
-echo "[+] Username is $username"
+echo "[+] Atempting to run: Get Caller Identity"
+if username="$(aws sts get-caller-identity --profile "$PROFILE" | jq -r '.Arn | split("/") | last')"; then
+    echo "[+] Username is $username"
+else
+    echo "[-] Error: Could not get username"
+fi
 
-policyARN="$(aws --profile="$PROFILE" iam list-attached-user-policies --user-name "$username" | jq -r '.AttachedPolicies[0].PolicyArn')"
-echo "[+] Policy ARN is $policyARN"
+echo "[+] Atempting to run: List Inline User Policies"
+if inlinePolicyARN="$(aws iam list-user-policies --profile "$PROFILE" --user-name "$username" | jq -r '.PolicyNames[]')"; then
+    echo "[+] Inline policy is $inlinePolicyARN"
+else
+    echo "[-] Error: Could not get inline policies"
+fi
 
-versionID="$(aws --profile "$PROFILE" iam get-policy --policy-arn "$policyARN" | jq -r '.Policy.DefaultVersionId')"
-echo "[+] Default Version ID is $versionID"
-echo "[+] Attempting to retrieve the policy"
-
-if ! aws --profile "$PROFILE" iam get-policy-version --policy-arn "$policyARN" --version-id "$versionID"; then
-    echo "[-] Error: Failed to retrieve user policy"
-    exit 1
+echo "[+] Atempting to run: List Attached User Policies"
+if policyARN="$(aws --profile="$PROFILE" iam list-attached-user-policies --user-name "$username" | jq -r '.AttachedPolicies[0].PolicyArn')"; then
+    echo "[+] Policy ARN is $policyARN"
+else
+    echo "[-] Error: Could not get Attached User Policies"
 fi
 
 
+echo "[+] Attempting to retrieve the policy"
+versionID="$(aws --profile "$PROFILE" iam get-policy --policy-arn "$policyARN" | jq -r '.Policy.DefaultVersionId')"
+echo "[+] Default Version ID is $versionID"
+
+
+
+if policy="$(aws --profile "$PROFILE" iam get-policy-version --policy-arn "$policyARN" --version-id "$versionID")"; then
+    echo "[+] Policy is $policy"
+else
+    echo "[-] Error: Could not get policy information"
+fi
